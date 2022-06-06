c4fe57f7aa9601814779373d7952d086d83a63e9
Dipesh Thapa <dipesh@naphix.com.au>
2022-06-06 10:14:31 +0545

    acf json

--- template-parts/section-test2.php
+++ template-parts/section-test2.php
@@ -1,5 +1,5 @@
 <?php  $columns = get_sub_field( 'columns' );
 foreach($columns as $column): ?>
-			<h4><?php echo $column['caption']?></h4>
-			<img src="<?php echo $column['img']['url']?>" alt="">
-		<?php endforeach; ?>
+<h4><?php echo $column['caption']?></h4>
+<img src="<?php echo $column['img']['url']?>" alt="">
+<?php endforeach; ?>
